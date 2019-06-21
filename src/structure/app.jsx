80386a0596ef2ecb52aaab407b9e30cd0caacd4d
Rob Butcher <robbutcher2001@gmail.com>
2019-06-21 16:54:10 +0100

    Function merge

--- src/structure/app.jsx
+++ src/structure/app.jsx
@@ -1,43 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class IngredientsPage extends Component {
-    constructor(props) {
-        super(props);
-
-        this.onButtonPress = this.onButtonPress.bind(this);
-    }
-
-    onButtonPress(event) {
-        event.preventDefault();
-        console.log('button pressed');
-        this.props.fireGetIngredientsRequest();
-    }
-
-    render() {
-        console.log(this.props.ingredients);
-        return (
-            <div>
-                <h3>Test page</h3>
-                <button onClick={event => this.onButtonPress(event)}>
-                    Press me to get ingredients
-                </button>
-                <ul>
-                    {this.props.ingredients.map((value, index) =>
-                        <li key={index}>{value}</li>
-                    )}
-                </ul>
-            </div>
-        );
-    }
-}
-
-const maptStateToProps = state => ({
+export default connect(state => ({
     ingredients: state.ingredients
-});
-
-const mapDispatchToProps = dispatch => ({
+}), dispatch => ({
     fireGetIngredientsRequest: payload => dispatch({ type: 'GET_INGREDIENTS_REQUEST', payload })
-});
+}))(
+    class IngredientsPage extends Component {
+        constructor(props) {
+            super(props);
 
-export default connect(maptStateToProps, mapDispatchToProps)(IngredientsPage);
+            this.onButtonPress = this.onButtonPress.bind(this);
+        }
+
+        onButtonPress(event) {
+            event.preventDefault();
+            console.log('button pressed');
+            this.props.fireGetIngredientsRequest();
+        }
+
+        render() {
+            console.log(this.props.ingredients);
+            return (
+                <div>
+                    <h3>Test page</h3>
+                    <button onClick={event => this.onButtonPress(event)}>
+                        Press me to get ingredients
+                    </button>
+                    <ul>
+                        {this.props.ingredients.map((value, index) =>
+                            <li key={index}>{value}</li>
+                        )}
+                    </ul>
+                </div>
+            );
+        }
+    }
+);
