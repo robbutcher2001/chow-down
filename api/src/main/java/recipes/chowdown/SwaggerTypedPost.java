689e81b7b761f298d0fd7b4f93e858e975aa0d17
Rob Butcher <robbutcher2001@gmail.com>
2019-10-23 20:51:51 +0100

    Contents

--- api/src/main/java/recipes/chowdown/SwaggerTypedPost.java
+++ api/src/main/java/recipes/chowdown/SwaggerTypedPost.java
@@ -9,7 +9,8 @@ import recipes.chowdown.domain.Recipe;
 
 public class SwaggerTypedPost implements RequestHandler<Recipe, ApigResponse> {
   public ApigResponse handleRequest(final Recipe recipe, final Context context) {
-    System.out.println(recipe);
+    System.out.println(recipe.getId());
+    System.out.println(recipe.getTitle());
 
     return new ApigResponse(HttpStatus.SC_OK, recipe.toString());
   }
