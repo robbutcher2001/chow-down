745c406d592a543f93b0e2a04cfb3c9cd7a6fdb3
Rob Butcher <robbutcher2001@gmail.com>
2019-10-24 20:43:18 +0100

    Try exception

--- api/src/main/java/recipes/chowdown/SwaggerTypedPost.java
+++ api/src/main/java/recipes/chowdown/SwaggerTypedPost.java
@@ -7,17 +7,22 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import org.apache.http.HttpStatus;
 
 import recipes.chowdown.domain.Recipe;
+import recipes.chowdown.exceptions.ResourcesNotFoundException;
 
 public class SwaggerTypedPost implements RequestHandler<Recipe, ApigResponse> {
 
   private static LambdaLogger Logger;
   
-  public ApigResponse handleRequest(final Recipe recipe, final Context context) {
+  public ApigResponse handleRequest(final Recipe recipe, final Context context) throws RuntimeException {
     Logger = context.getLogger();
 
     Logger.log("The recipe object:");
     Logger.log(recipe.getId());
     Logger.log(recipe.getTitle());
+
+    if (recipe.getId() == "exception") {
+      throw new ResourcesNotFoundException("can't find it");
+    }
 
     return new ApigResponse(HttpStatus.SC_CREATED, recipe);
   }
