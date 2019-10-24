ec2921159e9d1de55b9d2ba9cf9e3a8fb1dc8e92
Rob Butcher <robbutcher2001@gmail.com>
2019-10-24 11:48:51 +0100

    Understand Lambda input

--- api/src/main/java/recipes/chowdown/SwaggerTypedPost.java
+++ api/src/main/java/recipes/chowdown/SwaggerTypedPost.java
@@ -1,20 +1,39 @@
 package recipes.chowdown;
 
+import java.io.IOException;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.apache.http.HttpStatus;
 
 import recipes.chowdown.domain.Recipe;
 
-public class SwaggerTypedPost implements RequestHandler<Recipe, ApigResponse> {
-  public ApigResponse handleRequest(final Recipe recipe, final Context context) {
+public class SwaggerTypedPost implements RequestHandler<String, ApigResponse> {
+  public ApigResponse handleRequest(final String object, final Context context) {
+    // context.getLogger().
     System.out.println("The recipe object:");
-    System.out.println(recipe);
-    System.out.println("The recipe getters:");
-    System.out.println(recipe.getId());
-    System.out.println(recipe.getTitle());
+    System.out.println(object);
 
-    return new ApigResponse(HttpStatus.SC_OK, recipe.getTitle());
+    ObjectMapper o = new ObjectMapper();
+    JsonNode jsonNode = null;
+    try {
+      jsonNode = o.readTree(object);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    // Recipe recipe = (Recipe) object;
+
+    System.out.println("The recipe object:");
+    System.out.println(jsonNode.toString());
+    // System.out.println("The recipe getters:");
+    // System.out.println(recipe.getId());
+    // System.out.println(recipe.getTitle());
+
+    return new ApigResponse(HttpStatus.SC_OK, jsonNode.toString());
   }
 }
