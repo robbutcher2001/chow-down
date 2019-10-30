3b73ea83eeb3e1c646081d39644139ad38b33654
Rob Butcher <robbutcher2001@gmail.com>
2019-10-30 22:04:29 +0000

    Generate some recipes

--- api/mock/src/main/java/recipes/chowdown/GetRecipesServiceMock.java
+++ api/mock/src/main/java/recipes/chowdown/GetRecipesServiceMock.java
@@ -1,9 +1,11 @@
 package recipes.chowdown;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
+
+import com.github.javafaker.Faker;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,17 +17,34 @@ import recipes.chowdown.schema.Recipe;
 @RestController
 public class GetRecipesServiceMock implements ApiApi {
 
-    @Override
-    public ResponseEntity<List<Recipe>> apiRecipesGet() {
-        Recipe r = new Recipe();
-        r.setTitle("fake thing");
-        return new ResponseEntity<List<Recipe>>(Collections.singletonList(r), HttpStatus.OK);
+    final Faker faker;
+    final List<Recipe> recipes = new ArrayList<>();
+
+    public GetRecipesServiceMock() {
+        this.faker = new Faker();
+
+        for (int i = 0; i < 20; i++) {
+            Recipe recipe = new Recipe();
+            recipe.setId(this.faker.internet().uuid());
+            recipe.setTitle(this.faker.food().dish());
+            recipe.setDescription(this.faker.hitchhikersGuideToTheGalaxy().marvinQuote());
+            recipe.setRating(this.faker.number().numberBetween(0, 6));
+            recipe.setUrl(this.faker.internet().url() + "/" + this.faker.internet().domainWord());
+            recipe.setImage(this.faker.internet().image());
+
+            recipes.add(recipe);
+        }
     }
 
     @Override
-    public ResponseEntity<Void> apiRecipesPost(@Valid Recipe body) {
-        // TODO Auto-generated method stub
-        return null;
+    public ResponseEntity<List<Recipe>> apiRecipesGet() {
+        return new ResponseEntity<List<Recipe>>(this.recipes, HttpStatus.OK);
     }
-    
+
+    @Override
+    public ResponseEntity<Recipe> apiRecipesPost(@Valid Recipe recipe) {
+        recipe.setId(this.faker.internet().uuid());
+        this.recipes.add(recipe);
+        return new ResponseEntity<>(recipe, HttpStatus.ALREADY_REPORTED);
+    }
 }
