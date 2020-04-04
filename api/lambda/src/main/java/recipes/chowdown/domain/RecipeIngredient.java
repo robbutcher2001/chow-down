c1d2abd85b383deca7d2ba3c653bb1922b049cd8
Rob Butcher <robbutcher2001@gmail.com>
2020-04-04 15:12:53 +0100

    Model

---
+++ api/lambda/src/main/java/recipes/chowdown/domain/RecipeIngredient.java
@@ -0,0 +1,18 @@
+package recipes.chowdown.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecipeIngredient {
+    private Long quantity;
+    private String unitId;
+    private String ingredientId;
+}
