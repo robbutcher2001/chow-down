dd2d0f51601cea962293558a100f7e404a763a32
Rob Butcher <robbutcher2001@gmail.com>
2020-04-07 21:43:24 +0100

    Tap colour

--- src/components/InputStarRating/index.tsx
+++ src/components/InputStarRating/index.tsx
@@ -20,6 +20,7 @@ const Label = styled.label`
     font-family: Times;
     margin-right: 0.5rem;
     cursor: pointer;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   }
 
   .filled::before {
