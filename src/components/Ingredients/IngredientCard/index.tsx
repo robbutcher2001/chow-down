f9b3af024428bcce617c754dfa4569c62adfefe0
Rob Butcher <robbutcher2001@gmail.com>
2020-05-13 14:35:03 +0100

    Box shadow units and ingredients

--- src/components/Ingredients/IngredientCard/index.tsx
+++ src/components/Ingredients/IngredientCard/index.tsx
@@ -15,6 +15,9 @@ const IngredientCard = styled.li`
   border-left-color: #0693E3;
   margin: 0.5rem;
   padding: 0.75rem;
+  border-radius: 4px;
+  box-shadow: 0px 2px 2px 0 rgba(0,0,0,0.2);
+  box-sizing: border-box;
 `
 
 export default (props: IngredientCardProps) => (
--- src/components/Units/UnitCard/index.tsx
+++ src/components/Units/UnitCard/index.tsx
@@ -15,6 +15,9 @@ const UnitCard = styled.li`
   border-left-color: #0693E3;
   margin: 0.5rem;
   padding: 0.75rem;
+  border-radius: 4px;
+  box-shadow: 0px 2px 2px 0 rgba(0,0,0,0.2);
+  box-sizing: border-box;
 
   aside {
     font-size: 0.8rem;
